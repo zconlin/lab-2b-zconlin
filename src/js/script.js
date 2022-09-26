071e885571af95f586a999f7ed9392d86ed9262e
zconlin <zac.conlin@gmail.com>
2022-09-26 12:38:13 -0600

    Filtering and Sorting works!

--- src/js/script.js
+++ src/js/script.js
@@ -26,25 +26,15 @@ class Task {
     }
 }
 
-function preventSomeKeys(event){
-    input.addEventListener('keydown', e => {
-        if(e.shiftKey)
-          if(e.which == 188,190){
-          }
-      })
-    }
-
 let tasks = []
 let newArray = []
 readTasks()
 
 function updateStorage(newData) {
-    // ... update the local storage
     localStorage.setItem("database", JSON.stringify(newData))
 }
 
 function readStorage() {
-    // ... read from the local storage
     let jsonString = localStorage.getItem('database')
     let result = JSON.parse(jsonString) || []
     result = result.map(taskData => new Task(taskData))
@@ -56,10 +46,6 @@ function createTask(event) {
     let formData = new FormData(event.currentTarget)
     let json = Object.fromEntries(formData)
     let newTask = new Task({text: json.taskName, date: json.date, done: false, id: Date.now()})
-    
-    // let content = document.createdElement("strong")
-    // content.textContent = event
-    // document.getElementById("usercontent").appendChild(content)
 
     tasks.push(newTask)
     console.log("Added new task")
@@ -76,13 +62,13 @@ function readTasks() {
 
     // if (sort button is checked)
     if (document.getElementById("cb-sort").checked) {
-        newArray.sort(function(a, b){return new Date(a.date) - new Date(b.date)});
+        newArray.sort(function(a, b){return new Date(a.date) - new Date(b.date)})
         console.log("Sorted!")
     }
     
     // if (filter button is checked)
     if (document.getElementById("ft-sort").checked) {
-        newArray.filter(function(a, b){return new Date(a.date) - new Date(b.date)});
+        newArray = newArray.filter(task => task.done == false)
         console.log("Filtered!")
     }
 
@@ -113,6 +99,11 @@ function deleteTask(id) {
     updateStorage(tasks)
     readTasks()
 }
+
+// area.value = localStorage.getItem('area')
+// area.oninput = () => {
+//     localStorage.setItem('area', area.value)                     // No idea where to start
+// };
 
 function prevent(event){
     event.preventDefault();
